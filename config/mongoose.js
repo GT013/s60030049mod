41b0467f2368924f8637792f8eece5f993d0d0ab
GT013 <42264121+GT013@users.noreply.github.com>
2020-11-18 16:19:37 +0700

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = function(){
     mongoose.set('debug', true);
-    var db=mongoose.connect('mongodb://localhost/modbus');
+    var db=mongoose.connect('mongodb://192.168.2.29/modbus');
     require('../app/models/modbus.model');
     return db;
 }
